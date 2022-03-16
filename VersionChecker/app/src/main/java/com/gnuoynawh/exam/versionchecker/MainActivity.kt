58b282db5a968549ed26e18800911ae4c6327a0f
gnouynawh <bizxhy@gmail.com>
2022-03-16 11:15:06 +0900

    html 페이지에서 버전 읽어오기 (html 파싱)

--- VersionChecker/app/src/main/java/com/gnuoynawh/exam/versionchecker/MainActivity.kt
+++ VersionChecker/app/src/main/java/com/gnuoynawh/exam/versionchecker/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.gnuoynawh.exam.versionchecker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import org.jsoup.Jsoup
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        VersionChecker().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null)
+
+    }
+
+    class VersionChecker : AsyncTask<String, String, String>() {
+
+        override fun doInBackground(vararg str: String?): String {
+            var newVersion = ""
+
+            try {
+                val url = "https://gnuoynawh.github.io/version.html"
+                val doc = Jsoup.connect(url)
+                    .timeout(3000)
+                    .ignoreContentType(true)
+                    .execute()
+                    .body()
+
+                val ver = Gson().fromJson(doc, Version::class.java)
+                newVersion = ver.aos
+
+                Log.e("TEST", "doc.toString = $doc")
+                Log.e("TEST", "ios = ${ver.ios}")
+                Log.e("TEST", "aos = ${ver.aos}")
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return newVersion
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            Log.e("TEST", "result = $result")
+        }
     }
 }
---
+++ VersionChecker/app/src/main/java/com/gnuoynawh/exam/versionchecker/Version.kt
@@ -0,0 +1,3 @@
+package com.gnuoynawh.exam.versionchecker
+
+data class Version(val ios: String, val aos: String)
