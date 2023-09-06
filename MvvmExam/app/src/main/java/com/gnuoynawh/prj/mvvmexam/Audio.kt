6e3fae8ed57b10b7e81fdf9c72efd7578b715c0a
gnuoynawh <bizxhy@gmail.com>
2023-09-06 23:01:41 +0900

    [Android Architecture] MVVM 예제 프로젝트

---
+++ MvvmExam/app/src/main/java/com/gnuoynawh/prj/mvvmexam/Audio.kt
@@ -0,0 +1,9 @@
+package com.gnuoynawh.prj.mvvmexam
+
+/**
+ *  데이터 클래스 추가
+ */
+data class Audio(
+    val id: Long,
+    val fileName: String
+)
---
+++ MvvmExam/app/src/main/java/com/gnuoynawh/prj/mvvmexam/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.gnuoynawh.prj.mvvmexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gnuoynawh.prj.mvvmexam.databinding.ActivityMainBinding
+
+/**
+ * MVVM 예제
+ *  - 참고
+ *      https://jminie.tistory.com/168
+ *      https://velog.io/@dldmswo1209/Android-MVVM-Pattern-%EC%A0%81%EC%9A%A9-%EC%98%88%EC%A0%9C
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val mainViewModel: MainViewModel = MainViewModel()
+    private val adapter = ListAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // DataBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // init ViewModel
+        binding.viewModel = mainViewModel
+        binding.lifecycleOwner
+
+        // LiveData Observe!
+        mainViewModel.audioList.observe(this) {
+
+            // 데이터 변경시 리스트에 데이터 업데이트
+            adapter.setData(
+                it.toList(),
+                onItemClickListener = {
+                    Toast.makeText(this@MainActivity, "click!! Title = ${it.fileName}", Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
+
+        // init RecyclerView
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+
+        // Button Event
+        binding.addButton.setOnClickListener {
+            mainViewModel.addItem()
+        }
+    }
+}
