50783df5e6d604ca14982568f55b5b5d5ff9fd4e
gnuoynawh <bizxhy@gmail.com>
2025-01-15 20:27:14 +0900

    애니메이션, 스타일/테마 적용

--- BasicsCodelab/app/src/main/java/com/gnuoynawh/codelab/basics/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/gnuoynawh/codelab/basics/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.gnuoynawh.codelab.basics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -24,8 +28,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.coerceAtLeast
 import com.gnuoynawh.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -77,21 +83,34 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier.padding(vertical = Dp(4f), horizontal = Dp(8f))
     ) {
         var expanded by rememberSaveable { mutableStateOf(false) }
-        var extraPadding = if (expanded) Dp(48f) else Dp(0f)
+        val extraPadding by animateDpAsState(
+            if (expanded) Dp(48f) else Dp(0f),
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
 
-        Row (modifier = Modifier.padding(Dp(24f))) {
-            Column(
-                modifier = Modifier
+        Surface {
+            Row (modifier = Modifier.padding(Dp(24f))) {
+                Column(
+                    modifier = Modifier
                         .weight(1f)
-                        .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello ")
-                Text(text = name)
-            }
-            ElevatedButton(
-                onClick = { expanded = !expanded}
-            ) {
-                Text(text = if (expanded) "Show less" else "Show more")
+                        .padding(bottom = extraPadding.coerceAtLeast(Dp(0f)))
+                ) {
+                    Text(text = "Hello ")
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.headlineMedium.copy(
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                }
+                ElevatedButton(
+                    onClick = { expanded = !expanded}
+                ) {
+                    Text(text = if (expanded) "Show less" else "Show more")
+                }
             }
         }
     }
@@ -119,13 +138,18 @@ fun OnboardingScreen(
     }
 }
 
-//@Preview(showBackground = true, widthDp = 320)
-//@Composable
-//fun GreetingPreview() {
-//    BasicsCodelabTheme {
-//        Greetings()
-//    }
-//}
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
+@Composable
+fun GreetingPreview() {
+    BasicsCodelabTheme {
+        Greeting(name = "Android")
+    }
+}
 
 //@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 //@Composable
