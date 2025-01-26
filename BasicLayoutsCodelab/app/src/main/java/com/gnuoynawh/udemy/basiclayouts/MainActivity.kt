49475408c299e1b261a2f61e64ba899473ecb6e5
gnuoynawh <bizxhy@gmail.com>
2025-01-26 15:30:37 +0900

    검색창, 정렬, 카드

--- BasicLayoutsCodelab/app/src/main/java/com/gnuoynawh/udemy/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/gnuoynawh/udemy/basiclayouts/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.gnuoynawh.udemy.basiclayouts
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.gnuoynawh.udemy.basiclayouts.ui.theme.BasicLayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +42,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BasicLayoutsCodelabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    SearchBar()
                 }
             }
         }
@@ -31,17 +51,121 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.search))
+        },
         modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = Dp(56f))
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SearchBarPreview() {
     BasicLayoutsCodelabTheme {
-        Greeting("Android")
+        Surface {
+            SearchBar()
+        }
+    }
+}
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text:Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(Dp(88f))
+                .clip(CircleShape)
+        )
+        Text(
+            stringResource(text),
+            modifier = Modifier.paddingFromBaseline(top = Dp(22f), bottom = Dp(8f)),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AlignYourBodyElementPreview() {
+    BasicLayoutsCodelabTheme {
+        Surface {
+            AlignYourBodyElement(
+                drawable = R.drawable.ab1_inversions,
+                text = R.string.inversions,
+                modifier = Modifier.padding(Dp(8f))
+            )
+        }
+    }
+}
+
+@Composable
+fun FavoriteCard(
+    @DrawableRes drawable: Int,
+    @StringRes text:Int,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(Dp(255f))
+        ) {
+            Image(
+                painter = painterResource(drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(Dp(80f))
+            )
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(horizontal = Dp(16f))
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun FavoriteCardPreview() {
+    BasicLayoutsCodelabTheme {
+        Surface {
+            FavoriteCard(
+                drawable = R.drawable.fc2_nature_meditations,
+                text = R.string.nature_meditations,
+                modifier = Modifier.padding(Dp(8f))
+            )
+        }
     }
 }
